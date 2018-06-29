31c700d699a0a9abbb5f9b1d43fbda1ee52e5a48
Eri Osawa <osawaeri@users.noreply.github.com>
2018-06-29 19:41:32 +0900

    5

--- Assets/Script/Scene/System/StartScn.cs
+++ Assets/Script/Scene/System/StartScn.cs
@@ -9,10 +9,8 @@ public class StartScn : MonoBehaviour
     // PlayerPrefs保存切り替え判定
     public static bool prefsSave = false;
 
-    // オープニング実行判定
-    public static bool op = false;
-
-    /// 
+    // ステージクリア判定
+    public static int stageNumber = 0;
 
     void Awake()
     {
@@ -27,17 +25,70 @@ public class StartScn : MonoBehaviour
         if (Input.GetMouseButtonDown(0)){
             toNextScene(); // シーンをチェック
         }
+
     }
 
+    // 移動するシーンを確認
     void toNextScene()
     {
-            /// オープニングを実行していない場合
-            /// オープニング画面に移動し、オープニング実行済み
-            if (op == false)
-            {
-                SceneManager.LoadScene("Opening");
-                op = true;
-            }
+        switch (stageNumber){
+            case 0: 
+                SceneManager.LoadScene("Opening"); // オープニング
+                break;
+
+            case 1:
+                SceneManager.LoadScene("Stage1"); // ステージ1
+                break;
+
+            case 2:
+                SceneManager.LoadScene("Stage2"); // ステージ2
+                break;
+
+            case 3:
+                SceneManager.LoadScene("Stage3"); // ステージ3
+                break;
+
+            case 4:
+                SceneManager.LoadScene("Stage4"); // ステージ4
+                break;
+
+            case 5:
+                SceneManager.LoadScene("Stage5"); // ステージ5
+                break;
+
+            case 6:
+                SceneManager.LoadScene("Stage6"); // ステージ6
+                break;
+
+            case 7:
+                SceneManager.LoadScene("Stage7"); // ステージ7
+                break;
+
+            case 8:
+                SceneManager.LoadScene("Stage8"); // ステージ8
+                break;
+
+            case 9:
+                SceneManager.LoadScene("Stage9"); // ステージ9
+                break;
+
+            case 10:
+                SceneManager.LoadScene("Stage10"); // ステージ10
+                break;
+
+            case 11:
+                SceneManager.LoadScene("Stage11"); // ステージ11
+                break;
+
+            case 12:
+                SceneManager.LoadScene("Stage12"); // ステージ12
+                break;
+
+            default:
+                break;
+        }
     }
+
+
 
 }
