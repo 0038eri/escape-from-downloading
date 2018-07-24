9b9ea82bdceaa4de74d02479eae3a188fda9dc0c
Eri Osawa <osawaeri@users.noreply.github.com>
2018-07-25 03:17:10 +0900

    38

--- Assets/Script/Player/PlayerOperation.cs
+++ Assets/Script/Player/PlayerOperation.cs
@@ -4,56 +4,166 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerOperation : MonoBehaviour {
-	
-	// プレイヤー移動速度
-	public float speed = 0.7f;
-    // プレイヤー前進判定
+
+    // プレイヤー座標
+    public Vector3 playerPos;
+    // プレイヤー地面接触判定
+    private bool isPlayerCol;
+
+    // 前進速度
+    public float runSpeed;
+    // 前進判定
     public bool isRunning = false;
-	
-	void Update () {
+
+    // スライド速度
+    public float slideSpeed;
+    // スライド判定
+    private string slide;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += playerCenter; // シーン呼び出しごとに毎回プレイヤーを中央に
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void Update () {
+
+        playerPos = this.transform.position; // プレイヤー座標取得
 
         // 前進
-        if(isRunning=true){
-		this.transform.position -= transform.up * speed * Time.deltaTime;
+        if(isRunning==true){
+            RunningMethod();
+
+            // 右移動
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                playerRight();
+            }
+            // 左移動
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                playerLeft();
+            }
+
         }
 
-		// Spaceでジャンプ
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			this.transform.position += new Vector3 (0, 1.0f, 0);
-		}
-		// Aで右 
-		if (Input.GetKey (KeyCode.A)) {
-			transform.position -= transform.right * speed * Time.deltaTime;
-		}
-		// Dで左 
-		if (Input.GetKey (KeyCode.D)) {
-			transform.position += transform.right * speed * Time.deltaTime;
-		}
+        // ジャンプ
+        if (isPlayerCol == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerJump();
+            }
+        }
 		
 	}
 
+    // 前進メソッド
+    public void RunningMethod()
+    {
+        this.transform.position -= transform.up * runSpeed * Time.deltaTime;
+    }
+
+    // プレイヤー初期位置
+    void playerCenter (Scene scene,LoadSceneMode sceneMode) {
+        Vector3 centerPos = new Vector3(0.0f, 0.0f, 0.0f);
+        this.transform.position = centerPos;
+        slide = "center"; // 中央に位置する
+    }
+
+    // 右移動
+    void playerRight(){
+        
+        switch(slide){
+
+            // 中央である時
+            case "center":
+                playerPos.z = 1.0f;
+                this.transform.position = playerPos;
+                slide = "right";
+                break;
+
+            // 左であるとき
+            case "left":
+                playerPos.z = 0.0f;
+                this.transform.position = playerPos;
+                slide = "center";
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    // 左移動
+    void playerLeft(){
+
+        switch(slide){
+
+            // 中央であるとき
+            case "center":
+                playerPos.z = -1.0f;
+                this.transform.position = playerPos;
+                slide = "left";
+                break;
+
+            // 右であるとき
+            case "right":
+                playerPos.z = 0.0f;
+                this.transform.position = playerPos;
+                slide = "center";
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    // ジャンプ
+    void playerJump () {
+        playerPos.y += 1.5f; // Y軸1.5ジャンプ
+        this.transform.position = playerPos;
+    }
+
     // 衝突判定
-	void OnCollisionEnter(Collision col){
-		
-		// エネミーに当たると      
-		if (col.gameObject.tag == "Enemy") {
+    private void OnCollisionEnter(Collision collision)
+    {
+        isPlayerCol = true; // 地面に接している
+
+        // エネミーに当たると      
+        if (collision.gameObject.tag == "Enemy")
+        {
             PlayerState.playerHp--; // HPを1減らす
             GameObject.Find("PlayerManager").SendMessage("updatePlayerHp"); // HP更新
             Destroy(gameObject); // エネミー消滅
-		}
+        }
 
-		// コインに当たると
-		if(col.gameObject.tag=="Coin"){
-			GameObject.Find("MoneyManager").SendMessage("getCoin"); // 所持金1増やす
-			Destroy(gameObject); // コイン消滅
-		}
+        // コインに当たると
+        if (collision.gameObject.tag == "Coin")
+        {
+            GameObject.Find("MoneyManager").SendMessage("getCoin"); // 所持金1増やす
+            Destroy(gameObject); // コイン消滅
+        }
 
         // ゴールしたら
-        if (col.gameObject.tag == "Goal")
+        if (collision.gameObject.tag == "Goal")
         {
             GameObject.Find("PlayerManager").SendMessage("gameClear"); // ゲームクリア
         }
 
-	}
+        //Debug.Log("playerfield");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isPlayerCol = false; // 地面と接していない
+        //Debug.Log("playersky");
+    }
 
 }
